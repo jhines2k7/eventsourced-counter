aea95ad8573e1a8fef54c78eaca690bb93dbe6c5
James Hines <jameshines10@gmail.com>
2017-07-19 13:29:04 -0400

    Updates the build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,12 +14,9 @@ fi
 mkdir .tmp
 
 cp js/*.js .tmp
-
+ 
 # js compile and transform
 node_modules/.bin/webpack --config=webpack.config.js
-
-# modify the value of the domain object in the config file
-sed -i "s/domain: 'localhost'/domain: 'rocketscores-api'/g" dist/bundle.js
 
 # copy index.html to dist directory
 cp index.html dist
@@ -27,6 +24,6 @@ cp index.html dist
 # change the src property of the script tag to bundle.js
 sed -i 's/dist\/bundle.js/bundle.js/g' dist/index.html
 
-docker build -t jhines2017/riot-eventsourcing .
+docker build -t jhines2017/eventsourced-counter .
 
 date; echo;
